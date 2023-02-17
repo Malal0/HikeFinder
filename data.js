12f6b0530d396233d452ccb865259637eb94938b
UncomfortableChai <mj1055119@gmail.com>
2023-02-17 08:37:10 -0800

    added cards with javascript

---
+++ data.js
@@ -0,0 +1,26 @@
+export const data = [
+    {
+        image: "https://images.unsplash.com/uploads/141148589884100082977/a816dbd7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+        title: "Fløyen",
+        distance: 10.2,
+        duration: 5,
+        description: "Fløibanen is one of Norway's most visited attractions. At the top of Fløyen you will find a large and varied hiking area, spectacular views of the city.",
+        link: ""
+    },
+    {
+        image: "https://images.unsplash.com/photo-1533551037358-c8f7182cdb79?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
+        title: "Ulriken",
+        distance: 3.85,
+        duration: 2.8,
+        description: "Ulriken is the highest of the Seven Mountains that surround the city of Bergen, Norway. It has a height of 643 meters (2,110 ft) above sea level.",
+        link: ""
+    },
+    {
+        image: "https://images.unsplash.com/photo-1555838770-c321c525b004?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        title: "Landåsfjellet",
+        distance: 3.5,
+        duration: 2.5,
+        description: "Landåsfjellet is a lower plateau south of Ulriken/Haugavarden which offers numerous trails, and is almost a separate hiking region by its own.",
+        link: ""
+    },
+]
--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 /////////////////////////////////////////////////////////////////////
 //      VARIABLES
 /////////////////////////////////////////////////////////////////////
+import { data } from "/data.js";
 const nav = document.querySelector("nav");
+const cardsContainer = document.querySelector("#cards-container");
+const string = data.map(card => {
+    return `
+    <div class="card">
+        <img class="image"
+            src="${card.image}"
+            alt="${card.title} image" />
+        <p class="title">${card.title}</p>
+        <p class="length">Length: ${card.distance} km - Est. ${card.duration} h</p>
+        <p class="description">${card.description}</p>
+        <a class="link" href="${card.link}" target="_self">learn more</a>
+    </div>
+    `
+}).join("");
 
+cardsContainer.innerHTML = string;
 /////////////////////////////////////////////////////////////////////
 //      FUNCTIONS
 /////////////////////////////////////////////////////////////////////
